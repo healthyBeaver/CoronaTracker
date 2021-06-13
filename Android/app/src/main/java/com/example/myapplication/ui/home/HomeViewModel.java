f840836044583781f8b2ec44f365366896807f68
jjlee783 <jeijun1112@gmail.com>
2021-06-13 14:27:44 +0900

    android

---
+++ Android/app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+
+
+    public HomeViewModel() {
+
+    }
+
+
+}
